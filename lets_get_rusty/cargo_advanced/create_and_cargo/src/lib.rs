a7481a08892e8a5c0d9e8a43c6fae667d142ebad
Noor Mohammed B <noormohammed.b@gpckasaragod.ac.in>
2022-06-01 21:20:21 +0530

    Cargo Advanced
    release profiles
    documentation for library
    large projects with workspaces

---
+++ lets_get_rusty/cargo_advanced/create_and_cargo/src/lib.rs
@@ -0,0 +1,58 @@
+/*
+//! # My Crate
+//!
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient.
+
+/// Adds one to the number given.
+///
+/// #Examples
+///
+/// let arg;
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_create::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+/// let foo = bar;
+pub fn add_one(x: i32) -> i32 {
+    x + 1
+}
+ */
+
+//! # Art
+//!
+//! A library for modeling artistic concepts.
+
+pub use self::kinds::PrimaryColor;
+pub use self::kinds::SecondaryColor;
+pub use self::utils::mix;
+
+pub mod kinds {
+    /// The primary colors according to the RYB color model.
+    pub enum PrimaryColor {
+        Red,
+        Yellow,
+        Blue,
+    }
+
+    /// The secondary colors according to the RYB color model.
+    pub enum SecondaryColor {
+        Orange,
+        Green,
+        Purple,
+    }
+}
+
+pub mod utils {
+    use crate::kinds::*;
+
+    /// Combines two primary colors in equal amounts to create
+    /// a secondary color.
+    pub fn mix(c1: PrimaryColor, c2: PrimaryColor) -> SecondaryColor {
+        // --snip--
+        unimplemented!();
+    }
+}
---
+++ lets_get_rusty/cargo_advanced/workspace/adder/src/main.rs
@@ -0,0 +1,6 @@
+use add_one;
+use rand;
+
+fn main() {
+    println!("Hello, world! {}", add_one::add_one(3));
+}

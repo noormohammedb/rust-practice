a5792cb03bbfd5cca46d55c4aa56a6b75b923545
Noor Mohammed B <noormohammedb@protonmail.com>
2022-07-03 11:08:41 +0530

    WebServer Project
    - slow request simulation
    - using thread pool
    - structure for thread pool
    - channels for throw jobs
    - worker struct for spawn thread

--- project_server/src/bin/main.rs
+++ project_server/src/bin/main.rs
@@ -1,14 +1,21 @@
-use std::fs;
+use project_server::ThreadPool;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
+use std::time::Duration;
+use std::{fs, thread};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        // thread::spawn(|| {
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -21,8 +28,12 @@ fn handle_connection(mut stream: TcpStream) {
     println!("Request: \n{}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (http_status_line, file_name) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "index.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
